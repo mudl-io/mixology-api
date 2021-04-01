4f4826431df009af9976945ecdff9e83347be57d
oFrusch <owenc343@gmail.com>
2021-04-01 14:15:07 -0400

    added rigorous signup validations

--- backend/api/custom_user/views.py
+++ backend/api/custom_user/views.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import CustomUserSerializer, CustomTokenObtainPairSerializer
+from .models import CustomUser
 
 
 class CustomUserCreate(APIView):
@@ -12,6 +13,12 @@ class CustomUserCreate(APIView):
     authentication_classes = ()
 
     def post(self, request, format="json"):
+        validations = self.validate_data(request.data)
+
+        if not validations["is_valid"]:
+            # return 207 so I can display the error to the user on the front end
+            return Response(validations["error_message"], status=status.HTTP_207_MULTI_STATUS)
+
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
@@ -19,6 +26,23 @@ class CustomUserCreate(APIView):
                 json = serializer.data
                 return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def validate_data(self, data):
+        user = data['username']
+        email = data['email']
+
+        user_exists = CustomUser.objects.filter(username=user).count() > 0
+
+        if user_exists:
+            return {"is_valid": False, "error_message": "Username is already in use"}
+
+        email_exists = CustomUser.objects.filter(email=email).count()
+
+        if email_exists:
+            return {"is_valid": False, "error_message": "Email address is already in use"}
+
+        return {"is_valid": True, "error_message": ""}
+        
 
 
 class ObtainTokenPairWithUser(TokenObtainPairView):

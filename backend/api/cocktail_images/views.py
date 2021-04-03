8beb47b4ab5eaeec0ca43ff85df1f24d54493e09
oFrusch <owenc343@gmail.com>
2021-04-03 15:29:08 -0400

    cleanup

--- backend/api/cocktail_images/views.py
+++ backend/api/cocktail_images/views.py
@@ -2,7 +2,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status, permissions, viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
-import json
 
 from .models import CocktailImage
 from cocktails.models import Cocktail

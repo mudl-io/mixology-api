22eac11f3e98981f5cddfe89c39fa80d62f6cca3
oFrusch <owenc343@gmail.com>
2021-08-15 17:35:16 -0400

    update custom user to have comp uted followers count and add more filtering options to cocktails view

--- api/custom_user/serializers.py
+++ api/custom_user/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, Follower
 
 from profile_pictures.serializers import ProfilePictureSerializer
 
@@ -14,6 +14,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
     username = serializers.CharField()
     password = serializers.CharField(min_length=8, write_only=True)
     active_profile_picture = ProfilePictureSerializer(many=False, required=False)
+    is_followed = serializers.SerializerMethodField("is_being_followed")
 
     class Meta:
         model = CustomUser
@@ -26,12 +27,19 @@ class CustomUserSerializer(serializers.ModelSerializer):
             "created_cocktails_count",
             "viewed_cocktails_count",
             "active_profile_picture",
+            "profile_description",
+            "is_followed",
+            "followers_count",
+            "following_count",
         )
         read_only_fields = (
             "saved_cocktails_count",
             "created_cocktails_count",
             "viewed_cocktails_count",
             "active_profile_picture",
+            "is_followed",
+            "followers_count",
+            "following_count",
         )
         extra_kwargs = {"password": {"write_only": True}}
 
@@ -44,6 +52,22 @@ class CustomUserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         instance.save()
         return instance
+
+    """
+    This is an N+1 query waiting to happen. If there is every a moment where grabbing a lot of users
+    then this needs to be changed so that it doesn't make a lot of individual queries to get this 
+    flag for each relationship.
+    """
+
+    def is_being_followed(self, requested_user):
+        requesting_user = self.context["request"].user
+
+        if requesting_user.is_anonymous:
+            return False
+
+        return Follower.objects.filter(
+            follower=requesting_user, followee=requested_user
+        ).exists()
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
